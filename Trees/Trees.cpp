0df80e3b805fba6b554723f71f59f4a623a4d580
shobhitcs <shobhitchauhancs@gmail.com>
2022-11-15 18:08:29 +0530

    Quick sort added

--- Trees/Trees.cpp
+++ Trees/Trees.cpp
@@ -32,6 +32,11 @@ public:
     ~Tree();
     void insert(Node* root,int n);
     void Inorder(Node* root);
+    void Preorder(Node* root);
+    void Postorder(Node* root);
+    bool search(Node* rot,int n);
+    void dis_leaf(Node* rot);
+    void isfull_tree(Node* rot);
 };
 
 Tree::Tree()
@@ -82,6 +87,70 @@ void Tree::Inorder(Node* rot)
     // cout << root->data << endl;
     Inorder(rot->right);
 }
+void Tree::Preorder(Node* rot)
+{   
+    // printf("Hello");
+    if (!rot) {
+        return;
+    }
+    printf("%d ",rot->data);
+    Preorder(rot->left);
+    // cout << root->data << endl;
+    Preorder(rot->right);
+}
+void Tree::Postorder(Node* rot)
+{   
+    // printf("Hello");
+    if (!rot) {
+        return;
+    }
+    Postorder(rot->left);
+    // cout << root->data << endl;
+    Postorder(rot->right);
+    printf("%d ",rot->data);
+}
+
+bool Tree::search(Node* rot,int n){
+    if(rot==NULL){
+        return false;
+    }
+    if(rot->data==n){
+        return true;
+    }
+    if(n<rot->data){
+        return search(rot->left,n);
+    }
+    else if (n>rot->data)
+    {
+        return search(rot->right,n);
+    }
+    
+}
+void Tree::dis_leaf(Node* rot)
+{   
+    // printf("Hello");
+    if (!rot) {
+        return;
+    }
+    dis_leaf(rot->left);
+    if(rot->left==NULL && rot->right==NULL)
+    printf("%d ",rot->data);
+    // cout << root->data << endl;
+    dis_leaf(rot->right);
+}
+
+void Tree::isfull_tree(Node* rot)
+{   
+    // printf("Hello");
+    if (!rot) {
+        return;
+    }
+    isfull_tree(rot->left);
+    if((rot->left==NULL) ^ (rot->right==NULL))
+    printf("%d ",rot->data);
+    // cout << root->data << endl;
+    isfull_tree(rot->right);
+}
 int main(){
     Tree t;
     t.insert(t.root,2);
@@ -90,8 +159,15 @@ int main(){
     t.insert(t.root,1);
     t.insert(t.root,17);
     t.insert(t.root,13);
-    t.insert(t.root,35);
+    // t.insert(t.root,35);
     // printf("%d \n",(t.root->left)->data);
-    t.Inorder(t.root);
+    t.Postorder(t.root);
+    if(t.search(t.root,35))
+    printf("\nTRUE\n");
+    else 
+    printf("\nFALSE\n");
+   t.dis_leaf(t.root);
+    printf("\n\n");
+   t.isfull_tree(t.root);
     return 0;
 }
