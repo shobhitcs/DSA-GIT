7b0b5ef446a7c305f64213cdf301e9aa982e14ec
shobhitcs <shobhitchauhancs@gmail.com>
2022-11-15 18:56:04 +0530

    Merge sort added

---
+++ Sorting/merge_sort.cpp
@@ -0,0 +1,67 @@
+#include<stdio.h>
+void merge(int* arr,int l,int mid,int h){
+    const int n1=mid-l+1,n2=h-mid;
+    int arrleft[n1];
+    int arrright[n2];
+
+
+    for (int i = 0; i < n1; i++)
+    {
+        arrleft[i]=arr[l+i];
+    }
+    for (int i = 0; i < n1; i++)
+    {
+        arrright[i]=arr[ mid+1+i];
+    }
+
+    int j=0,k=l,i=0;
+    while (i<n1 && j<n2)
+    {
+        if(arrleft[i]<=arrright[j] ){
+            arr[k]=arrleft[i];
+            i++;k++;
+        }
+        else{
+            arr[k]=arrright[j];
+            j++;k++;
+        }
+    }
+
+    while (i<n1)
+    {
+        arr[k]=arrleft[i];
+        i++;k++;
+    }
+    while (j<n2)
+    {
+        arr[k]=arrright[j];
+        j++;k++;
+    }
+    
+    
+    
+
+}
+
+
+void mergesort(int* arr,int l,int h){
+    if(l<h){
+        int mid=(l+h)/2;
+        mergesort(arr,l,mid);
+        mergesort(arr,mid+1,h);
+        merge(arr,l,mid,h);
+
+    }
+}
+
+int main(){
+    int a[7]={5,4,3,2,1,-0,-3};
+    int size=7;
+    mergesort(a,0,size-1);
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d ",*(a+i));
+    }
+    
+    return 0;
+}
